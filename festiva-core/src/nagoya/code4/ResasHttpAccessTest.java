2ac21447b39b416becb1bda6e061fdac90be6043
梅澤 耕一 <kouichiume@umezawa-no-MacBook-Air.local>
2016-12-04 13:25:11 +0900

    commit first

---
+++ festiva-core/src/nagoya/code4/ResasHttpAccessTest.java
@@ -0,0 +1,22 @@
+package nagoya.code4;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.apache.http.ParseException;
+import org.junit.Test;
+
+public class ResasHttpAccessTest {
+
+	@Test
+	public void testSendString() throws ParseException, IOException {
+		
+		ResasHttpAccess.sendString("ttes");
+	
+		
+		
+		
+	}
+
+}
