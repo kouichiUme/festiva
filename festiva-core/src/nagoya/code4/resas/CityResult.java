803b0ed131ac57792c06d8d8d6eec7c388a1968a
kouichiUme <kouichiume@gmail.com>
2017-01-29 20:34:54 +0900

    industry and pref city

---
+++ festiva-core/src/nagoya/code4/resas/CityResult.java
@@ -0,0 +1,35 @@
+package nagoya.code4.resas;
+
+public class CityResult {
+	
+	private String prefCode;
+	private String cityCode;
+	private String cityName;
+	private String bigCityFlag;
+	
+	public String getPrefCode() {
+		return prefCode;
+	}
+	public void setPrefCode(String prefCode) {
+		this.prefCode = prefCode;
+	}
+	public String getCityCode() {
+		return cityCode;
+	}
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public String getBigCityFlag() {
+		return bigCityFlag;
+	}
+	public void setBigCityFlag(String bigCityFlag) {
+		this.bigCityFlag = bigCityFlag;
+	}
+
+}
---
+++ festiva-core/src/nagoya/code4/resas/IndustryNarrowResult.java
@@ -0,0 +1,37 @@
+package nagoya.code4.resas;
+
+public class IndustryNarrowResult {
+	
+	private String siscCode;
+	
+	private String simcCode;
+	
+	private String siscName;
+
+	public String getSiscCode() {
+		return siscCode;
+	}
+
+	public void setSiscCode(String siscCode) {
+		this.siscCode = siscCode;
+	}
+
+	public String getSimcCode() {
+		return simcCode;
+	}
+
+	public void setSimcCode(String simcCode) {
+		this.simcCode = simcCode;
+	}
+
+	public String getSiscName() {
+		return siscName;
+	}
+
+	public void setSiscName(String siscName) {
+		this.siscName = siscName;
+	}
+	
+	
+
+}
