bb81afd5d71332d3bd031c5c9ffb6c6ca0eaeb69
kouichi <kouichiume@gmail.com>
2017-08-02 01:01:31 +0900

    出生数・死亡数／転入数・転出数

---
+++ festiva-core/src/nagoya/code4/resas/PopulationsumEstimateLabel.java
@@ -0,0 +1,15 @@
+package nagoya.code4.resas;
+
+public class PopulationsumEstimateLabel extends AbstractResultList<PopulationsumEstimateResultData> {
+	
+	private String label;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+}
