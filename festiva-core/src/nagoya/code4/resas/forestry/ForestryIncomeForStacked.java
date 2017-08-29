14128605e59c6e72e6f4fbf487e8697d4a098bc9
kouichi <kouichiume@gmail.com>
2017-08-29 23:14:03 +0900

    林作業請負収入（経営体あたり） コミット漏れ

--- festiva-core/src/nagoya/code4/resas/forestry/ForestryIncomeForStacked.java
+++ festiva-core/src/nagoya/code4/resas/forestry/ForestryIncomeForStacked.java
@@ -1,67 +1,17 @@
 package nagoya.code4.resas.forestry;
 
-import java.util.List;
 
-public class ForestryIncomeForStacked {
-	
-	private String prefCode;
-	
-	private String prefName;
-	
-	private String cityCode;
-	
-	private String cityName;
+
+public class ForestryIncomeForStacked extends AbstractForestryIncome<ForestryIncomeForStackedResultData> {
 	
 	private String label;
 	
-	private List<ForestryIncomeForStackedResultData> years;
-
-	public String getPrefCode() {
-		return prefCode;
-	}
-
-	public void setPrefCode(String prefCode) {
-		this.prefCode = prefCode;
-	}
-
-	public String getPrefName() {
-		return prefName;
-	}
-
-	public void setPrefName(String prefName) {
-		this.prefName = prefName;
-	}
-
-	public String getCityCode() {
-		return cityCode;
-	}
-
-	public void setCityCode(String cityCode) {
-		this.cityCode = cityCode;
-	}
-
-	public String getCityName() {
-		return cityName;
-	}
-
-	public void setCityName(String cityName) {
-		this.cityName = cityName;
-	}
-
 	public String getLabel() {
 		return label;
 	}
 
 	public void setLabel(String label) {
 		this.label = label;
-	}
-
-	public List<ForestryIncomeForStackedResultData> getYears() {
-		return years;
-	}
-
-	public void setYears(List<ForestryIncomeForStackedResultData> years) {
-		this.years = years;
 	}
 	
 }
--- festiva-core/src/nagoya/code4/resas/forestry/ForestryIncomeForStackedRequest.java
+++ festiva-core/src/nagoya/code4/resas/forestry/ForestryIncomeForStackedRequest.java
@@ -1,28 +1,8 @@
 package nagoya.code4.resas.forestry;
 
-public class ForestryIncomeForStackedRequest {
+public class ForestryIncomeForStackedRequest extends AbstractForestryIncomeRequest {
 	
 	public static final String url ="/forestry/income/forStacked";
 
-	
-	private String cityCode;
-	
-	private String prefCode;
-
-	public String getCityCode() {
-		return cityCode;
-	}
-
-	public void setCityCode(String cityCode) {
-		this.cityCode = cityCode;
-	}
-
-	public String getPrefCode() {
-		return prefCode;
-	}
-
-	public void setPrefCode(String prefCode) {
-		this.prefCode = prefCode;
-	}
 	
 }
